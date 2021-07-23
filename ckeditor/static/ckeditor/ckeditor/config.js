9bcc807d3ccf86ac73e11c0a7250c1d7192d09c1
henriqueandras <36888073+henriqueandras@users.noreply.github.com>
2021-07-23 14:56:22 -0300

    Update config.js

--- ckeditor/static/ckeditor/ckeditor/config.js
+++ ckeditor/static/ckeditor/ckeditor/config.js
@@ -7,4 +7,27 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+	
+	// Enable a limited set of text formats:
+    config.format_tags = 'p;title;caption';
+    config.format_title = {
+        name: 'Title',
+        element: 'h2',
+        styles: {
+            'font-weight': '700',
+            'margin-top': '60px',
+            'margin-bottom': '30px',
+        }
+    };
+    config.format_caption = {
+        name: 'Image Caption',
+        element: 'p',
+        styles: {
+            'font-size': '14px',
+            'font-style': 'italic',
+            'padding': '10px',
+            'text-align': 'center',
+            'color': '#BFBFBF',
+        }
+    };
 };
